ac4f4c6ea26df1842ae8039efbd26cf3d2b281df
Annisah Husni Daulay <138286798+bandelkid@users.noreply.github.com>
2024-01-27 09:23:46 +0700

    Menampilkan Namabarang dan Harga

--- belanja.kt
+++ belanja.kt
@@ -1,13 +1,19 @@
 //fungsi dengan return
 fun barang(item : String):Int{
     return when (item){
-    	"Baju" -> 100000
+    	"Gamis" -> 150000
+        "Kaos" -> 50000
+        "Kemeja" -> 700000
+        "Celana" -> 120000
+        "Kaos Dalam" -> 20000
+        
         else -> 0
     } 
 }
 
 fun main() {
+    val namaBarang = "Kaos"
+    val harga = barang(namaBarang)
+    println("Baju yang anda beli $namaBarang harganya adalah Rp. $harga")
     
-    val harga = barang("Baju")
-    println(harga)
 }
