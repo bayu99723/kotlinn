98797306858405f51b9aed3dee76783098c46a7a
Annisah Husni Daulay <138286798+bandelkid@users.noreply.github.com>
2024-01-27 10:31:44 +0700

    Membuat Total Bayar

--- belanja.kt
+++ belanja.kt
@@ -15,13 +15,20 @@ fun diskon(harga: Int, diskon: Double): Int{
     return (harga * diskon).toInt()
     
 }
+fun total(harga: Int, hargaDiskon: Int): Int{
+    return (harga - hargaDiskon).toInt()
+    
+}
 
 fun main() {
     val namaBarang = "Gamis"
-    val diskon = 0.1
+    val diskon = 0.05
     val harga = barang(namaBarang)
     val hargaDiskon= if (harga>100000) diskon(harga,diskon) else 0
+    val hargaTotal = total(harga, hargaDiskon)
     println("Baju yang anda beli: \nNama barang\t: $namaBarang \nHarga Barang\t: Rp. $harga")
     println("Diskon \t\t: Rp. $hargaDiskon")
+    println("Total Bayar \t: Rp. $hargaTotal")
     
 }
+
